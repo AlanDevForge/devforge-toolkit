3f0fa0d3555d722cc6c7d4bc642d98c8b6926805
AlanDevForge <alan@devforge.ie>
2026-07-09 15:36:16 +0100

    fix: support Pester 3 tests

--- tests/DevForgeToolkit.Tests.ps1
+++ tests/DevForgeToolkit.Tests.ps1
@@ -1,28 +1,28 @@
-BeforeAll {
-    Import-Module "$PSScriptRoot\..\DevForgeToolkit.psd1" -Force
-    $repo = Open-DevForgeRepository "D:\DevForge\Git\devforge-hq"
-}
-
 Describe "DevForgeToolkit" {
+    $modulePath = Join-Path $PSScriptRoot "..\DevForgeToolkit.psd1"
+    Import-Module $modulePath -Force
+
+    $repo = Open-DevForgeRepository "D:\DevForge\Git\devforge-hq"
+
     It "loads the module" {
-        Get-Command -Module DevForgeToolkit | Should -Not -BeNullOrEmpty
+        Get-Command -Module DevForgeToolkit | Should Not BeNullOrEmpty
     }
 
     It "opens a DevForge repository" {
-        $repo | Should -Not -BeNullOrEmpty
-        $repo.Entities.Count | Should -BeGreaterThan 0
+        $repo | Should Not BeNullOrEmpty
+        $repo.Entities.Count | Should BeGreaterThan 0
     }
 
     It "returns machine entities" {
-        Get-DevForgeMachine -Repository $repo | Should -Not -BeNullOrEmpty
+        Get-DevForgeMachine -Repository $repo | Should Not BeNullOrEmpty
     }
 
     It "returns product entities" {
-        Get-DevForgeProduct -Repository $repo | Should -Not -BeNullOrEmpty
+        Get-DevForgeProduct -Repository $repo | Should Not BeNullOrEmpty
     }
 
     It "validates the repository" {
         $result = Test-DevForgeRepository -Repository $repo
-        $result.Passed | Should -BeTrue
+        $result.Passed | Should Be $true
     }
 }
